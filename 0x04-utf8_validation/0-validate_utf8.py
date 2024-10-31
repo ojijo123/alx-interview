f86461b7d654c65677b62e16bce9b38405e02a2c
ojijo123 <oumaerick173@gmail.com>
2024-10-31 18:54:36 +0000

    validate utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
 """UTF-8 Validation"""
 
+
+def get_leading_set_bits(num):
+    """wiil return the number of leading set bits (1)"""
+    set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        set_bits += 1
+        helper = helper >> 1
+    return set_bits
+
+
 def validUTF8(data):
-    # Number of bytes remaining in the current UTF-8 character
-    bytes_to_process = 0
-
-    # Masks to determine the number of bytes in a character
-    mask_1_byte = 0b10000000  # 1st bit
-    mask_2_bytes = 0b11100000  # 3 leading bits
-    mask_3_bytes = 0b11110000  # 4 leading bits
-    mask_4_bytes = 0b11111000  # 5 leading bits
-    mask_continuation = 0b11000000  # For 10xxxxxx check
-
-    for byte in data:
-        # Only keep the last 8 bits of each integer
-        byte &= 0xFF
-
-        if bytes_to_process == 0:
-            # Determine the number of bytes for the current character
-            if (byte & mask_1_byte) == 0:
-                # 1-byte character (0xxxxxxx)
-                bytes_to_process = 0
-            elif (byte & mask_2_bytes) == 0b11000000:
-                # 2-byte character (110xxxxx)
-                bytes_to_process = 1
-            elif (byte & mask_3_bytes) == 0b11100000:
-                # 3-byte character (1110xxxx)
-                bytes_to_process = 2
-            elif (byte & mask_4_bytes) == 0b11110000:
-                # 4-byte character (11110xxx)
-                bytes_to_process = 3
-            else:
-                # Invalid start byte
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    bits_count = 0
+    for i in range(len(data)):
+        if bits_count == 0:
+            bits_count = get_leading_set_bits(data[i])
+            '''1-byte (format: 0xxxxxxx)'''
+            if bits_count == 0:
+                continue
+            '''a character in UTF-8 can be 1 to 4 bytes long'''
+            if bits_count == 1 or bits_count > 4:
                 return False
         else:
-            # Check if byte is a valid continuation byte (10xxxxxx)
-            if (byte & mask_continuation) != 0b10000000:
+            '''checks if current byte has format 10xxxxxx'''
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
                 return False
-            bytes_to_process -= 1
-
-    # All characters should have been processed completely
-    return bytes_to_process == 0
+        bits_count -= 1
+    return bits_count == 0
